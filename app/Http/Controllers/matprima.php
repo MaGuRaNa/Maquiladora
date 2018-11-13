8fa4c94f247e173ffc63c519726b28659be70c92
arturo166 <arturo.artmon@gmail.com>
2018-11-13 03:09:00 -0600

    Se realizaron modificaciones en controlador materia prima para realizar modificaciones en el sistema

--- app/Http/Controllers/matprima.php
+++ app/Http/Controllers/matprima.php
@@ -65,7 +65,11 @@ class matprima extends Controller
           
             $matp->Fecha=$ad;
 			$matp->save();
-		return redirect('/altamateria');//Redireccion a la ruta
+		    $proceso ="ALTA REALIZADA";
+	     	$mensaje ="Registro guardado correctamente";
+	     	return view('sistema_vistas.mensaje')
+	    	->with('proceso',$proceso)
+		    ->with('mensaje',$mensaje);//Redireccion a la ruta
 	}		
 	
 	public function reportemateria()
@@ -77,76 +81,55 @@ class matprima extends Controller
 	->with('materia_primas',$materia_primas);
 	
 	}
-	public function eliminam($idm)
+	public function eliminamateria($Id_mat)
 	{
-		    maestros::find($idm)->delete();
+		    maestros::find($Id_mat)->delete();
 		    $proceso = "ELIMINAR MAESTROS";
 			$mensaje = "El maestro ha sido borrado Correctamente";
 			return view ('sistema.mensaje')
 			->with('proceso',$proceso)
 			->with('mensaje',$mensaje);
 	}
-	public function modificam($idm)
+	public function modificamateria($Id_mat)
 	{
-		$maestro = maestros::where('idm','=',$idm)->get();
-        $idc = $maestro[0]->idc;
-        $carrera = carreras::where('idc','=',$idc)->get();
-        $demascarreras = carreras::where('idc','!=',$idc)->get();
-		return view('sistema.guardamaestro')
-	                ->with('maestro',$maestro[0])
-                    ->with('idc',$idc)
-                    ->with('carrera',$carrera[0]->nombre)
-                    ->with('demascarreras',$demascarreras);
+		$materia = materia_primas::where('Id_mat','=',$Id_mat)->get();
+		return view('sistema_vistas.modificamateriaprima')
+                    ->with('materia',$materia[0]);
 	}
     
-    	public function editamaestro(Request $request) //Request porque recibe todo el formulario
+    	public function editamateria(Request $request) //Request porque recibe todo el formulario
 	{
-		$nombre = $request->nombre;
-		$idm = $request->idm;
-		$edad= $request->edad;
-		$sexo = $request->sexo;
-		$beca= $request->beca;
-		$cp = $request->cp;
+		$Id_mat = $request->idmat;
+		$Nom_mat = $request->nommat;
+		$Desc= $request->desc;
+		$Cantidad = $request->cant;
+		$Peso= $request->pes;
+        
 		///NUNCA SE RECIBEN LOS ARCHIVOS
 		
 		
 		$this->validate($request,[
-	     'idm'=>'required|numeric',
-		 'nombre'=>'required',['regex:/^[A-Z][A-Z,a-z, ,ñ,á,é,í,ó,ú]+$/'],
-		 'edad'=>'required|integer|min:18|max:60',
-		 'cp'=>'required',['regex:/^[0-9]{5}$/'],
-		 'beca'=>'required',['regex:/^[0-9]+[.][0-9]{2}$/'],
-		 'archivo'=>'image|mimes:jpg,jpeg,png,gif'
+	     'idmat'=>'required|numeric',
+		 'nommat'=>'required|regex:/^[A-Z][A-Z,a-z, ,ñ,é,í,á,ó,ú]*$/',
+		 'desc'=>'required|regex:/^[A-Z,0-9,a-z, ,ñ,é,í,á,ó,ú]*$/',
+		 'cant'=>'required|numeric',
+		 'pes'=>'required|regex:/^[0-9]+[.][0-9]{2}$/',
 	     ]);
-            
-            $file = $request->file('archivo');
-	 if($file!="")
-	 {
-	 $ldate = date('Ymd_His_');
-	 $img = $file->getClientOriginalName();
-	 $img2 = $ldate.$img;
-	 \Storage::disk('local')->put($img2, \File::get($file));
-	 }
 		 
-		 
+        
+        $ad= Carbon::now()->toDateString();
+        
 		 //insert into maestros(idm,nombre,edad,sexo) values('$idm',
 		 //'$nombre')
-		    $maest = maestros::find($idm);
-			$maest->idm = $request->idm;
-			$maest->nombre = $request->nombre;
-			$maest->edad =$request->edad;
-			$maest->sexo= $request->sexo;
-			$maest->cp=$request->cp;
-			$maest->beca=$request->beca;
-			$maest->idc=$request->idc;
-            if($file!=''){
-                $maest->archivo=$img2;
-            }
-			$maest->save();
-		$proceso = "MODIFICACIÓN DE MAESTRO";	
-	    $mensaje="Registro modificado correctamente";
-		return view('sistema.mensaje')
-		->with('proceso',$proceso)
-		->with('mensaje',$mensaje);
+		    $matp = materia_primas::find($Id_mat);
+			$matp->Id_mat = $request->idmat;
+			$matp->Nom_mat = $request->nommat;
+			$matp->Descripcion =$request->desc;
+			$matp->Cantidad= $request->cant;
+			$matp->Peso=$request->pes;
+          
+            $matp->Fecha=$ad;
+			$matp->save();
+		   return redirect('/reportemateria');
 	}
 }
